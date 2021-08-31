7d169caa329b68e628a5483da0c688a6c99da056
inori-lover <337938188@qq.com>
2021-09-01 00:05:09 +0800

    feat: 可以分隔文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
 import { inori } from './inori';
+import { App } from './app';
 
 const app = document.createElement('div');
 document.body.append(app);
-
-function App() {
-  return <h1>Hello world!</h1>;
-}
 
 inori.render(<App />, app);
